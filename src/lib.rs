e5d9edc11795ebdbe56cbe930711d4444a8107e2
Tatsuya Takamura <takamura@tonchidot.com>
2016-09-13 06:38:15 +0900

    Try cargo test

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+pub mod sandbox {
+  pub fn add(x: i32, y: i32) -> i32 {
+    x + y
+  }
+
+  #[test]
+  fn add_test() {
+    assert!(add(3, 5) == 8);
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+extern crate binary_nn;
+
 fn main() {
+  println!("3 + 5 = {}", binary_nn::sandbox::add(3, 5));
   println!("Hello, world!");
 }
