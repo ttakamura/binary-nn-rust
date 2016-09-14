72295d1040b4b0795fc89b9fa5f0c7d7bc4e91cf
Tatsuya Takamura <takamura@tonchidot.com>
2016-09-15 04:45:40 +0900

    Plan bit vector struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,24 @@ pub mod sandbox {
   pub fn add(x: i32, y: i32) -> i32 {
     x + y
   }
+}
 
+pub mod bnn {
+  pub struct BitVec {
+    storage: Vec<u32>,
+    nbits: usize,
+    unit_size: usize
+  }
 
+  pub fn get(&self, i: usize) -> Option<bool> {
+    if i >= self.nbits {
+      return None;
+    }
+    let w = i / self.unit_size;
+    let b = i % self.unit_size;
+    let x = self.storage.get(w).unwrap();
+    return (x & (1 << b)) != 0;
+  }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 extern crate binary_nn;
 
 fn main() {
-  let x: i32 = 1;
-  let y: i32 = 4;
-  println!("{:?}", x | y);
-
   println!("3 + 5 = {}", binary_nn::sandbox::add(3, 5));
   println!("Hello, world!");
 }
