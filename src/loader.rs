18cfcd4b07ed85216370c68d516ffa3b30bbe395
Tatsuya Takamura <takamura@tonchidot.com>
2017-01-26 18:02:11 +0900

    Load as BitMatrix

--- src/loader.rs
+++ src/loader.rs
@@ -1,9 +1,29 @@
 use std::f32;
-use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 use std::error::Error;
 use std::mem;
+
+extern crate binary_nn;
+use binary_nn::backend::bitmatrix::BitMatrix2;
+use binary_nn::backend::bitmatrix_trait::*;
+
+pub fn load_f32_as_bitmatrix(path: String, nrow: u32, ncol: u32) -> BitMatrix2 {
+  let f32_vec = load_f32(path);
+  let mut bit_mat = BitMatrix2::falses((nrow, ncol));
+  let mut idx = 0;
+  for irow in 0..nrow {
+    for icol in 0..ncol {
+      if f32_vec[idx] > 0.0 {
+        bit_mat.set_true((irow, icol));
+      } else {
+        bit_mat.set_false((irow, icol));
+      }
+      idx += 1
+    }
+  }
+  return bit_mat;
+}
 
 pub fn load_f32(path: String) -> Vec<f32> {
   let mut buffer: Vec<u8> = Vec::new();
@@ -32,6 +52,6 @@ pub fn pack(buffer: &[u8]) -> f32 {
   buf[2] = buffer[2];
   buf[3] = buffer[3];
   let x: f32 = unsafe { mem::transmute(buf) };
-  println!("x is {:#?}", x);
+  //  println!("x is {:#?}", x);
   return x;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 extern crate binary_nn;
+use binary_nn::backend::bitmatrix_trait::*;
+
 mod loader;
 
 fn main() {
-  let result = loader::load_f32("data/binary_net.l1.b.dat".to_string());
-  println!("result length is {}", result.len());
+  let result = loader::load_f32_as_bitmatrix("data/binary_net.l1.W.dat".to_string(), 1000, 784);
+  println!("result[0, 0]   {}", result.get((0, 0)));
+  for i in 0..100 {
+    println!("result[{}, 783] {}", i, result.get((i, 783)));
+  }
+  println!("result[999, 783] {}", result.get((999, 783)));
 }
