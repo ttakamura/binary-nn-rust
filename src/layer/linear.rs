aa57fdbcb3ffa61b4d78e6f8177e5bdd57eb5135
Tatsuya Takamura <takamura@tonchidot.com>
2017-02-10 10:46:39 +0900

    forward_u8

--- src/layer/linear.rs
+++ src/layer/linear.rs
@@ -1,4 +1,5 @@
 use backend::bitmatrix::BitMatrix2;
+use backend::bitmatrix_trait::*;
 use loader;
 
 pub struct BinaryLinearLayer {
@@ -11,7 +12,21 @@ impl BinaryLinearLayer {
     return BinaryLinearLayer { weight: weight };
   }
 
-  pub fn forward_u8(&self, x: &Vec<u8>) -> Vec<u8> {
-    return Vec::new();
+  pub fn forward_u8(&self, x: &Vec<u8>) -> Vec<i32> {
+    let mut z = Vec::new();
+    let (nrow, ncol) = self.weight.len();
+    for irow in 0..nrow {
+      let mut val = 0i32;
+      for icol in 0..ncol {
+        if self.weight.get((irow, icol)) {
+          val += x[icol as usize] as i32;
+        } else {
+          val -= x[icol as usize] as i32;
+        }
+      }
+      // println!("{}, {}", irow, val);
+      z.push(val);
+    }
+    return z;
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ use binary_nn::layer::linear::*;
 
 fn main() {
   let l1 = BinaryLinearLayer::load("data/binary_net.l1.W.dat".to_string(), 1000, 784);
-  println!("result[0, 0]   {}", l1.weight.get((0, 0)));
-  println!("result[999, 783] {}", l1.weight.get((999, 783)));
+  println!("l1[0, 0]   {}", l1.weight.get((0, 0)));
+  println!("l1[999, 783] {}", l1.weight.get((999, 783)));
+
+  let z = l1.forward_u8(&vec![8u8; 784]);
+  println!("z[0] {}", z[0]);
+  println!("z[999] {}", z[999]);
 
   let bn = BatchNormLayer::load("data/binary_net.b1.dat".to_string(), 1000);
-  println!("bn len {}", bn.len());
+  println!("bn.len {}", bn.len());
 }
